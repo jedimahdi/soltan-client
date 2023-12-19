4186390a30b110f1f8d5c59cbcb9b795b823ef80
Mahdi Seyedan <mahdi.se@yahoo.com>
2023-12-19 05:55:14 +0330

    update nix flake and change version of purescript to 0.15.10

--- flake.nix
+++ flake.nix
@@ -2,48 +2,37 @@
   description = "Soltan Client";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
     easy-purescript-nix = {
       url = "github:justinwoo/easy-purescript-nix";
       flake = false;
     };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, easy-purescript-nix, ... }@inputs:
-    let
-      name = "halogen-realworld";
-
-      supportedSystems = [
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-    in
-    {
-      devShell = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-
-          easy-ps = import easy-purescript-nix { inherit pkgs; };
-        in
-        pkgs.mkShell {
-          inherit name;
-          buildInputs = (with pkgs; [
-            nodejs-16_x
-            nixpkgs-fmt
-          ]) ++ (with easy-ps; [
-            purs
-            purs-tidy
-            psa
-            spago
-            purescript-language-server
-          ]);
-        });
-    };
+  outputs = { self, nixpkgs, utils, easy-purescript-nix, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        easy-ps = import easy-purescript-nix { inherit pkgs; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (_: _: { inherit (easy-ps) purs-tidy pulp spago purescript-language-server; purescript = easy-ps.purs-0_15_10; })
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell
+          {
+            name = "soltan-client";
+            buildInputs = with pkgs; [ nodejs-18_x purs-tidy purescript-language-server spago purescript nodePackages.prettier gmp ];
+            shellHook = ''
+              export LD_LIBRARY_PATH="${
+                pkgs.lib.makeLibraryPath
+                (with pkgs; [ stdenv.cc.cc gmp zlib ncurses5 ])
+              }:$LD_LIBRARY_PATH"
+            '';
+          };
+      }
+    );
 }
